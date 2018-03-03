2f9338a55cab48506f3cdd53765cba9a6b4308bf
darkroom51 <darkroom@wp.pl>
2018-03-03 13:53:24 +0100

    installed react-router-dom, material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import Paper from 'material-ui/Paper'
+import TodoLists from './components/TodoLists'
+
+const paperStyles = {
+    margin: 20,
+    padding: 20,
+    textAlign: 'center'
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       Hello World of App
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="container">
+                    <AppBar
+                        style={{textAlign: 'center'}}
+                        showMenuIconButton={false}
+                        title="ToDo Ur Life"
+                    />
+                    <Paper style={paperStyles}>
+                        <TodoLists/>
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/TodoLists.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoLists extends Component {
+    render() {
+        return (
+            <div>
+                Hello World of TodoLists
+            </div>
+        );
+    }
+}
+
+export default TodoLists;
